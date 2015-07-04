ca03250e8f1ebeeb494d1f6b91931e551c12ed1d
fsites <fsites521@gmail.com>
2015-07-04 12:34:19 -0700

    working on reset functions

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ var app = angular.module('waitstaff', [])
 app.controller('MyCtrl', ['$scope', function($scope) {
 
 	$scope.clearform = function(){
-		$scope.meal-price = "";
+		$scope.mealPrice = "";
 		$scope.tax = "";
 		$scope.tip = "";
 	}
 
+	$scope.inititalCharges = function() {
+		$scope.subtotal = 0;
+		$scope.tip = 0;
+		$scope.total = 0;
+	}
 
+	$scope.initalEarnings = function() {
+		$scope.mealCount = 0;
+		$scope.tipTotal = 0;
+		$scope.averageTip = 0;
+	}
+
+	$scope.initalState = function (){
+		$scope.clearform();
+		$scope.initalCharges();
+		$scope.initalEarnings();
+	}
+	
 }]);
 
 
