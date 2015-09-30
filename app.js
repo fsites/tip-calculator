46f44a715fc7070c0476d576a20082b246479d4f
fsites <fsites521@gmail.com>
2015-09-29 19:48:12 -0700

    made js file a bit easier to read.

---
+++ app.js
@@ -0,0 +1,64 @@
+angular.module('waitstaff', ['ngRoute'])
+
+	.config(function($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl : 'home.html',
+			controller: 'HomeCtrl'
+		})
+		.when('/input', {
+			templateUrl : 'input.html',
+			controller : 'InputCtrl'
+		})
+		.when('/earnings', {
+			templateUrl : 'earnings.html',
+			controller : 'EarningsCtrl'
+		})
+		.otherwise('/');
+	})
+	.controller('HomeCtrl', ['$scope', function($scope) {
+		//empty
+	}])
+	.controller('InputCtrl', ['$scope', function($scope) {
+
+		$scope.clearform = function() {
+			$scope.mealPrice = "";
+			$scope.taxPercent = 9.5; //Local tax rate
+			$scope.tipPercent = "";
+		};
+
+		$scope.initialCharges = function() {
+			$scope.subTotal = 0;
+			$scope.tip = 0;
+			$scope.total = 0;
+		};
+
+		$scope.initialEarnings = function() {
+			$scope.mealCount = 0;
+			$scope.tipTotal = 0;
+			$scope.averageTip = 0;
+		};
+
+		$scope.initialState = function () {
+			$scope.clearform();
+			$scope.initialCharges();
+			$scope.initialEarnings();
+		};
+
+		$scope.onSubmit = function() {
+			$scope.mealCount++;
+			$scope.tipTotal += $scope.tip;
+			$scope.averageTip = $scope.tipTotal / $scope.mealCount;
+			$scope.clearform();
+		};
+
+		$scope.initialState(); //loads initial state on page load
+	}])
+	.controller('EarningsCtrl', ['$scope', function($scope) {
+		$scope.$watchGroup(['mealPrice', 'taxPercent', 'tipPercent'], function(newValues, oldValues, scope) {
+			$scope.subTotal = $scope.mealPrice + (($scope.taxPercent / 100) * $scope.mealPrice);
+			$scope.tip = $scope.mealPrice / 100 * $scope.tipPercent;
+			$scope.total = $scope.subTotal + $scope.tip;
+		});
+	}]);
+
+
