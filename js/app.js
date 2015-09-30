2f5c6d0b7a299a4bf0653a1bfb8efd4e01f7fcbe
fsites <fsites521@gmail.com>
2015-09-29 19:45:28 -0700

    routing works for all 3 pages. App is a bit broken and needs some css redone but work in progress

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ angular.module('waitstaff', ['ngRoute'])
 			controller : 'InputCtrl'
 		})
 		.when('/earnings', {
-			templateUrl : 'earnings.html'
+			templateUrl : 'earnings.html',
 			controller : 'EarningsCtrl'
 		})
 		.otherwise('/');
@@ -24,41 +24,41 @@ angular.module('waitstaff', ['ngRoute'])
 			$scope.mealPrice = "";
 			$scope.taxPercent = 9.5; //Local tax rate
 			$scope.tipPercent = "";
-		}
+		};
 
 		$scope.initialCharges = function() {
 			$scope.subTotal = 0;
 			$scope.tip = 0;
 			$scope.total = 0;
-		}
+		};
 
 		$scope.initialEarnings = function() {
 			$scope.mealCount = 0;
 			$scope.tipTotal = 0;
 			$scope.averageTip = 0;
-		}
+		};
 
 		$scope.initialState = function () {
 			$scope.clearform();
 			$scope.initialCharges();
 			$scope.initialEarnings();
-		}
+		};
 
 		$scope.onSubmit = function() {
 			$scope.mealCount++;
 			$scope.tipTotal += $scope.tip;
 			$scope.averageTip = $scope.tipTotal / $scope.mealCount;
 			$scope.clearform();
-		}
+		};
 
 		$scope.initialState(); //loads initial state on page load
 	}])
 	.controller('EarningsCtrl', ['$scope', function($scope) {
-		$scope.$watchGroup(['mealPrice', 'taxPercent', 'tipPercent'], function(newValues, oldValues, scope) {
+		$scope.$watchGroup(['mealPrice', 'taxPercent', 'tipPercent'], function(newValues, oldValues, $scope) {
 			$scope.subTotal = $scope.mealPrice + (($scope.taxPercent / 100) * $scope.mealPrice);
 			$scope.tip = $scope.mealPrice / 100 * $scope.tipPercent;
 			$scope.total = $scope.subTotal + $scope.tip;
-		})
+		});
 	}]);
 
 
