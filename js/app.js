a1b2c27202bcb17e93bd79aae3eb4e0794e4fecf
fsites <fsites521@gmail.com>
2015-10-23 19:21:28 -0700

    using a factory

--- js/app.js
+++ js/app.js
@@ -12,67 +12,48 @@ var waitstaff = angular.module('waitstaff', ['ngRoute'])
 		})
 		.when('/earnings', {
 			templateUrl : 'earnings.html',
-			controller : 'InputCtrl'
+			controller : 'EarningsCtrl'
 		})
 		.otherwise('/');
 	})
-	.service('Data', function() {
-		return {};
+	.factory('Total', function() {
+		var earnings = {
+			tipTotal: 0,
+			mealCount: 0,
+			averageTip: 0
+		}
+		return {
+			get: function() {
+				return earnings;
+			},
+			add: function(tip) {
+				earnings.tipTotal += tip;
+				earnings.mealCount++;
+				earnings.averageTip = earnings.tipTotal / earnings.mealCount
+			}
+			reset: function() {
+				earnings.tipTotal = 0;
+				earnings.mealCount = 0;
+				earnings.averageTip = 0;
+			}
+		}
 	})
 	.controller('HomeCtrl', ['$scope', function($scope) {
 		//empty
 	}])
-	.controller('InputCtrl', ['$scope', 'Data', function($scope, Data) {
-		$scope.data = Data;
+	.controller('InputCtrl', ['$scope', 'Total', function($scope, Total) {
+		initialState(); //resets on page load
 
-		$scope.clearform = function() {
-			$scope.data.mealPrice = '';
-			$scope.data.taxPercent = 9.5; //Local tax rate
-			$scope.data.tipPercent= '';
-		};
-
-		$scope.initialCharges = function() {
-			$scope.data.subTotal = 0;
-			$scope.data.tip = 0;
-			$scope.data.total = 0;
-		};
-
-		$scope.initialEarnings = function() {
-			$scope.data.mealCount = 0;
-			$scope.data.tipTotal = 0;
-			$scope.data.averageTip = 0;
-		};
-
-		$scope.onSubmit = function() {
-			$scope.data.mealCount++;
-			console.log('meal count is ' + $scope.data.mealCount);
-			$scope.data.tipTotal += $scope.data.tip;
-			console.log('tip total is ' + $scope.data.tipTotal);
-			$scope.data.averageTip = $scope.data.tipTotal / $scope.data.mealCount;
-			console.log('average tip is ' + $scope.data.averageTip)
-			$scope.clearform();
-		};
-
-		$scope.initialState = function () {
-			$scope.clearform();
-			console.log($scope.data.taxPercent);
-			$scope.initialCharges();
-			$scope.initialEarnings();
-		};
-
-		$scope.initialState(); //loads initial state on page load
-
-		$scope.$watchGroup(['data.mealPrice', 'data.taxPercent', 'data.tipPercent'], function(newValues, oldValues, $scope) {
-			$scope.data.subTotal = $scope.data.mealPrice + (($scope.data.taxPercent / 100) * $scope.data.mealPrice);
-			$scope.data.tip = $scope.data.mealPrice / 100 * $scope.data.tipPercent;
-			$scope.data.total = $scope.data.subTotal + $scope.data.tip;
-		});
-
+		$
 
 	}])
-	.controller('EarningsCtrl', ['$scope', 'Data', function($scope, Data) {
-		$scope.data = Data;
+	.controller('EarningsCtrl', ['$scope', 'Total', function($scope, Total) {
+		$scope.data = Total.get();
 
+		$scope.reset = function() {
+			Total.reset();
+			$scope.data = Total.get();
+		}
 	}]);
 
 
